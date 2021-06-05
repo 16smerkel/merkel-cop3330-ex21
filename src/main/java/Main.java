e1f2cc14e355b4efa6bffc83e0f1c15584989aeb
16smerkel <16smerkel@gmail.com>
2021-06-05 11:58:03 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number of the month:");
+        int theNum = scanner.nextInt();
+        String theMonth;
+        switch(theNum)
+        {
+            case 1:
+                theMonth = "January";
+                break;
+            case 2:
+                theMonth = "February";
+                break;
+            case 3:
+                theMonth = "March";
+                break;
+            case 4:
+                theMonth = "April";
+                break;
+            case 5:
+                theMonth = "May";
+                break;
+            case 6:
+                theMonth = "June";
+                break;
+            case 7:
+                theMonth = "July";
+                break;
+            case 8:
+                theMonth = "August";
+                break;
+            case 9:
+                theMonth = "September";
+                break;
+            case 10:
+                theMonth = "October";
+                break;
+            case 11:
+                theMonth = "November";
+                break;
+            case 12:
+                theMonth = "December";
+                break;
+            default:
+                theMonth = "N/A";
+        }
+        System.out.println("The name of the month is " + theMonth + ".");
+    }
+}
